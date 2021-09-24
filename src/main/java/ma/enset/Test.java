4aa3ba07632fa04c7a01726eff0493791b1eb132
thebestofAKB <abdoulkarimbalde25@gmail.com>
2021-09-24 20:36:44 +0000

    comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,7 +1,11 @@
+/**
+ * Utilisation de l'outil Git
+ */
 package ma.enset;
 
 public class Test {
     public static void main(String[] args) {
+        //----------------------------------
         System.out.println("I love java");
     }
 }
